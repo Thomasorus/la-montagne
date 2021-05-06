ba33b967b6fcafa183cd36d3fbfd3f2aea91d5e3
Thomasorus <contact@thomasorus.com>
2021-05-06 20:42:13 +0200

    Added error page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1 v-if="error.statusCode === 404">Cette page n'existe pas</h1>
+    <h1 v-else>Une erreur est survenue</h1>
+    <p>
+      Vous avez effectué une recherche ? Désolé, cette démo n'a pas de backend
+      permettant de vous afficher un résultat !
+    </p>
+    <p>
+      Vous pouvez <NuxtLink to="/">retourner à l'accueil</NuxtLink> ou
+      <NuxtLink to="/annonces">consulter les annonces</NuxtLink>.
+    </p>
+  </main>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error' // you can set a custom layout for the error page
+}
+</script>
