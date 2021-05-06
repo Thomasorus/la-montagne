2fd780e3c91ec3eb5f434eea651aac8049cacc07
Thomasorus <contact@thomasorus.com>
2021-05-07 00:02:12 +0200

    Implemented dynamic route generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,35 @@
+import axios from "axios"
+
+const dynamicRoutes = async () => {
+  const resForAnnonces = await axios.get('http://localhost:3001/annonces')
+  const routesForAnnonces = resForAnnonces.data.map((annonce) => {
+    return {
+      route: `/annonce?slug=${annonce.slug}`,
+      payload: annonce
+    }
+  })
+  return routesForAnnonces
+}
+
 export default {
   components: true,
     css: [
       '~/assets/css/reset.css', '~/assets/css/global.css'
     ],
     modules: [
-      '@nuxtjs/proxy'
+      '@nuxtjs/proxy', '@nuxtjs/axios'
     ],
     proxy: {
       '/geo/': { target: 'https://api-adresse.data.gouv.fr/search/', pathRewrite: {'^/geo/': ''}, changeOrigin: true }  
     },
-    buildModules: [
-      'nuxt-vite'
-    ],
     buildDir: 'dist',
-    build: {
-      parallel: false,
-      cache: false,
-      hardSource: false,
+    // build: {
+    //   parallel: false,
+    //   cache: false,
+    //   hardSource: false,
+    // },
+    target: 'static',
+    generate: {
+      routes:dynamicRoutes
     }
   }
